fee2cdfd4166a900fa1b948a667fcdbc0e7e2e0b
Web <Web@Secretariat2.cananet.priv>
2019-02-01 12:37:40 +0100

    Premier pas
    Fichier necessaire pour commencer la création d'un thème wordpress

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="<?php bloginfo('language'); ?>">
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
